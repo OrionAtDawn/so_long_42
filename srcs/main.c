d0b99672395d14fc80f3096f0120c62b30ad30a2
edufour/Elodie <lalou_dufour@hotmail.com>
2023-05-25 09:29:19 -0400

    Cleaned files

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: edufour <edufour@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 09:21:54 by edufour           #+#    #+#             */
-/*   Updated: 2023/05/17 13:51:19 by edufour          ###   ########.fr       */
+/*   Updated: 2023/05/25 09:23:05 by edufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv)
 	t_data	info;
 
 	if (argc != 2)
-		return (1);
+		return (error_message("No file was provided."));
 	info.path = argv[1];
 	if (check_map(argv[1], &info) == -1)
 		return (1);
